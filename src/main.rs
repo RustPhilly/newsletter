c70e4e2b581d5e4b7c0cd64f5b50bd1b474f9fbe
Darrow O'Lykos <caffeine-yeeter@protonmail.com>
2022-03-19 22:18:54 -0400

    enable telemetry (tracing and logging)
    OpenTelemetry compatible.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,27 @@
 //! main.rs
 
+mod configuration;
 mod routes;
 mod startup;
+mod telemetry;
 
-use newsletter::configuration::get_configuration;
+use configuration::get_configuration;
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use startup::run;
 use std::net::TcpListener;
+use telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber("newsletter".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres.");
+    let connection_pool =
+        PgPool::connect(&configuration.database.connection_string().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind to random port");
     run(listener, connection_pool)?.await
--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,9 @@
 use actix_web::dev::Server;
+use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 use crate::routes::{health_check, subscribe};
 
@@ -10,6 +12,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
 
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
